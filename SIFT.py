81079ce0aab67b8b2ac421b4a0708f8d63f06f31
hantyou <daniel.zhai@yahoo.com>
2020-03-23 18:18:17 +0800

    second commit, finished all octaves' Keypoints detection.

--- SIFT.py
+++ SIFT.py
@@ -16,31 +16,102 @@ def GeneratePyrPics(I, LayerNum):
     pyrPics.append(I0)
     pyrPics.append(I)
     for i in range(2, LayerNum):
-        pyrPics.append(cv2.pyrDown(pyrPics[i - 1]))
+        pyrPics.append(MyBiLiResize(pyrPics[i - 1], 1.5))
+        # pyrPics.append(cv2.pyrDown(pyrPics[i - 1]))
     return pyrPics
 
 
-def GenerateGausPyrPics(I, LayerNum):
+def GenerateGausPyrPics(I, LayerNum, scale=1.5):
     pyrPics = []
     As = []
     Bs = []
+    DoGs = []
     LayerNum += 1
     for i in range(LayerNum):
+
         tempA = Double1D_GaussianBlur(I, 7, 1.414213)
         tempB = Double1D_GaussianBlur(tempA, 7, 1.414213)
         pyrPics.append(I)
         As.append(tempA)
         Bs.append(tempB)
-        I = cv2.pyrDown(Bs[i])
-    return [pyrPics, As, Bs]
+        DoGs.append(tempA - tempB)
+        # I = MyBiLiResize(Bs[i], scale)
+        I = cv2.resize(Bs[i], (int(Bs[i].shape[0] / scale), int(Bs[i].shape[1] / scale)))
+        if i == 0:
+            I = cv2.resize(Bs[i], (int(Bs[i].shape[0] / 2), int(Bs[i].shape[1] / 2)))
+
+        # I = cv2.pyrDown(Bs[i], (Bs[i].shape[0] / 1.5, Bs[i].shape[0] / 1.5))
+    return [pyrPics, As, Bs, DoGs]
 
 
-def ShowPicPyr(pyrPics):
+def ShowPicPyr(string, pyrPics):
     len = pyrPics.__len__()
     for i in range(len):
-        cv2.namedWindow("Pic" + str(i), cv2.WINDOW_NORMAL)
-        cv2.imshow("Pic" + str(i), pyrPics[i])
+        cv2.namedWindow(string + "Pic" + str(i), cv2.WINDOW_NORMAL)
+        cv2.imshow(string + "Pic" + str(i), pyrPics[i])
         print(pyrPics[i].shape)
+
+
+def FindMaximaMinima(I):
+    """此处在循环中使用函数调用严重影响算法速度
+    def Judge8(I, j, i):
+        tp = I[j - 1:j + 2, i - 1:i + 2]
+        maxima = np.max(tp)
+        minima = np.min(tp)
+        if maxima == tp[1,1] or minima == tp[1,1]:
+            return 1
+        else:
+            return 0
+    """
+    [h, w] = [I.shape[0], I.shape[1]]
+    maxout = np.zeros((h, w))
+    for i in range(1, w - 1):
+        for j in range(1, h - 1):
+            tp = I[j - 1:j + 2, i - 1:i + 2]
+            maxima = np.max(tp)
+            minima = np.min(tp)
+            if maxima == tp[1, 1] or minima == tp[1, 1]:
+                flag = 1
+            else:
+                flag = 0
+            maxout[j, i] = flag
+    return maxout
+
+
+def EliminateScaleNonKeys(MaxMinFlag, CurrentOctave=0, scale=1.5):
+    def DeepSearchEli(MaxMinFlag, y, x, o, scale):
+        len = MaxMinFlag.__len__()
+        [cy, cx] = [MaxMinFlag[o].shape[0], MaxMinFlag[o].shape[1]]
+        y1 = int(y / scale)
+        x1 = int(x / scale)
+        if o == 1:
+            y1 = int(y / 2)
+            x1 = int(x / 2)
+        if CurrentOctave == len:
+            """
+            if MaxMinFlag[o][y1, x1] == 0:
+                return MaxMinFlag[o][y1, x1]
+            else:
+                return 1
+            """
+            return MaxMinFlag[o][y1, x1]
+        else:
+            if MaxMinFlag[o][y1, x1] == 1:
+                MaxMinFlag[o][y1, x1] = DeepSearchEli(MaxMinFlag, y1, x1, o + 1, scale)
+            else:
+                MaxMinFlag[o][y1, x1] = 0
+            return MaxMinFlag[o][y1, x1]
+
+    len = MaxMinFlag.__len__()
+    yxs = np.where(MaxMinFlag[CurrentOctave] == 1)
+    ys = yxs[0]
+    xs = yxs[1]
+    n = ys.__len__()
+    for k in range(n):
+        y = ys[k]
+        x = xs[k]
+        MaxMinFlag[CurrentOctave][y, x] = DeepSearchEli(MaxMinFlag, y, x, CurrentOctave + 1, scale)
+    return MaxMinFlag
 
 
 def Direct2D_GaussianBlur(I, SizeOfGaussianKernel=3, sigma=1.414213):
@@ -149,22 +220,36 @@ def MyBiLiResize(I, factor):
 
 
 I = cv2.imread("28.jpg")
+I = cv2.resize(I, (512, 512))
 gray = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
 I = I / 255
 gray = gray / 255
+I = gray
 I0 = cv2.pyrUp(I)
 # I = cv2.resize(I, (2048, 2048))
 pyrPics = []
 pyrPics.append(I)
 OctaveNum = CalcPyrNum([I.shape[0], I.shape[1]])
-[pyrPics, As, Bs] = GenerateGausPyrPics(I0, OctaveNum)
+scale = 1.5
+[pyrPics, As, Bs, DoGs] = GenerateGausPyrPics(I0, OctaveNum, scale=scale)
 # pyrPics = GeneratePyrPics(I, OctaveNum)
-ShowPicPyr(pyrPics)
+# ShowPicPyr("pyr", pyrPics)
+AbsDoGs = np.abs(DoGs)
+ShowPicPyr("AbsDoGs", AbsDoGs)
+ShowPicPyr("pyrPics", pyrPics)
+cv2.waitKey(0)
+MaxMinFlag = []
+for i in range(OctaveNum):
+    MaxMinFlag.append(FindMaximaMinima(AbsDoGs[i]))
+MaxMinFlagEli = EliminateScaleNonKeys(MaxMinFlag.copy(), CurrentOctave=0, scale=scale)
+ShowPicPyr("Flag Eliminated Scale wise unmatchable ", MaxMinFlagEli)
+# ShowPicPyr("DoGs ", DoGs)
+ShowPicPyr("Flag ", MaxMinFlag)
 GI = Direct2D_GaussianBlur(I, 7, 1.414)
 I1d = Double1D_GaussianBlur(I, 7, 1.414)
 # myresizetest = MyBiLiResize(I1d, 2)
 # sysresize = MyResize(I1d, 2)
-IdiffGaussian = DiffGaussian(I, 7, 7, sigma1=1.414213, sigma2=1.414213)
+IdiffGaussian = np.abs(DiffGaussian(I, 7, 7, sigma1=1.414213, sigma2=1.414213))
 Myimshow("Original", I)
 Myimshow("Difference", IdiffGaussian)
 """
